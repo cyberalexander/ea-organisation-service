6a61600e11250e4c300bd63fe928a4f785dd48fb
CyberAlexander <taurum13@gmail.com>
2023-03-04 10:15:15 +0300

    Organisation entity turned from Java record to a Java class due to Hibernate restrictions

--- src/main/java/com/eagle/eye/organisation/model/Organisation.java
+++ src/main/java/com/eagle/eye/organisation/model/Organisation.java
@@ -29,10 +29,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
- * Created : 04/12/2022 11:29
+ * Created : 04/03/2023 10:08
  * Project : ea-organisation-service
  * IDE : IntelliJ IDEA
  *
@@ -41,11 +42,96 @@ import java.util.UUID;
  */
 @Entity
 @Table(name = "ea_organisations")
-public record Organisation(
-        @Id @Column(name = "organisation_id", nullable = false) UUID id,
-        @Column(name = "name", nullable = false) String name,
-        @Column(name = "contact_name", nullable = false) String contactName,
-        @Column(name = "contact_email", nullable = false) String contactEmail,
-        @Column(name = "contact_phone", nullable = false) String contactPhone
-) {
+public class Organisation {
+
+    @Id
+    @Column(name = "organisation_id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "contact_name", nullable = false)
+    private String contactName;
+
+    @Column(name = "contact_email", nullable = false)
+    private String contactEmail;
+
+    @Column(name = "contact_phone", nullable = false)
+    private String contactPhone;
+
+    public Organisation() {
+    }
+
+    public Organisation(UUID id, String name, String contactName, String contactEmail, String contactPhone) {
+        this.id = id;
+        this.name = name;
+        this.contactName = contactName;
+        this.contactEmail = contactEmail;
+        this.contactPhone = contactPhone;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organisation that = (Organisation) o;
+        return id.equals(that.id) && Objects.equals(name, that.name) && Objects.equals(contactName, that.contactName) && Objects.equals(contactEmail, that.contactEmail) && Objects.equals(contactPhone, that.contactPhone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Organisation{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", contactName='" + contactName + '\'' +
+                ", contactEmail='" + contactEmail + '\'' +
+                ", contactPhone='" + contactPhone + '\'' +
+                '}';
+    }
 }
